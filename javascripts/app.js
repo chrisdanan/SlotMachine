437120190f751a3c602a82fc16c6f61ab10faaae
Christopher Danan <chrisddan@gmail.com>
2015-06-27 08:34:19 -0700

    Alter code on how images are appended to the slot machine and initialize slots before timer starts. -Img clones are now appended to the slot machine in order for duplicates to appear at the same time. -Slots now initialize with images immediately after Spin button is pressed.

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,8 +1,8 @@
-var $azumangaDaioh = $("<img>").attr("id", "azumangaDaioh").addClass("slotImage").attr("src", "assets/images/AzumangaDaioh.jpg").attr("alt", "Azumanga Daioh slot image"),
-	$toraDora = $("<img>").attr("id", "toraDora").addClass("slotImage").attr("src", "assets/images/ToraDora.jpg").attr("alt", "Toradora slot image"),
-	$swordArtOnline = $("<img>").attr("id", "swordArtOnline").addClass("slotImage").attr("src", "assets/images/SwordArtOnline.jpg").attr("alt", "Sword Art Online slot image"),
-	$spiritedAway = $("<img>").attr("id", "spiritedAway").addClass("slotImage").attr("src", "assets/images/SpiritedAway.jpg").attr("alt", "Spirited Away slot image"),
-	$paranoiaAgent = $("<img>").attr("id", "paranoiaAgent").addClass("slotImage").attr("src", "assets/images/ParanoiaAgent.jpg").attr("alt", "Paranoia Agent slot image");
+var $azumangaDaioh = $("<img>").addClass("slotImage azumangaDaioh").attr("src", "assets/images/AzumangaDaioh.jpg").attr("alt", "Azumanga Daioh slot image"),
+	$toraDora = $("<img>").addClass("slotImage toraDora").attr("src", "assets/images/ToraDora.jpg").attr("alt", "Toradora slot image"),
+	$swordArtOnline = $("<img>").addClass("slotImage swordArtOnline").attr("src", "assets/images/SwordArtOnline.jpg").attr("alt", "Sword Art Online slot image"),
+	$spiritedAway = $("<img>").addClass("slotImage spiritedAway").attr("src", "assets/images/SpiritedAway.jpg").attr("alt", "Spirited Away slot image"),
+	$paranoiaAgent = $("<img>").addClass("slotImage paranoiaAgent").attr("src", "assets/images/ParanoiaAgent.jpg").attr("alt", "Paranoia Agent slot image");
 
 var slotElements = [$azumangaDaioh, $toraDora, $swordArtOnline, $spiritedAway, $paranoiaAgent],
 	leftTimer, middleTimer, rightTimer;
@@ -10,6 +10,9 @@ var slotElements = [$azumangaDaioh, $toraDora, $swordArtOnline, $spiritedAway, $
 var randomElement = function(){
 	//Reference: http://stackoverflow.com/questions/1527803/generating-random-numbers-in-javascript-in-a-specific-range
 	var randElement =  slotElements[Math.floor(Math.random() * slotElements.length)];
+	console.log(randElement);
+
+	randElement = randElement.clone();
 
 	return randElement;
 };
@@ -43,13 +46,20 @@ var main = function(){
 		$spinButton = $("#spinButton");
 
 	$spinButton.on("click", function(){
+		console.log("Spin button clicked");
+		//Initialize slots. The timer will only initialize the slots at the set time interval.
+		// Instead, we want the items to appear immediately once the button is clicked.
+		$leftSlot.append(randomElement());
+		$middleSlot.append(randomElement());
+		$rightSlot.append(randomElement());
+
 		$leftSlot.toggleClass("spinning");
 		$middleSlot.toggleClass("spinning");
 		$rightSlot.toggleClass("spinning");
 
-		leftTimer = chooseSlotItem($leftSlot, 200);
-		middleTimer = chooseSlotItem($middleSlot, 100);
-		rightTimer = chooseSlotItem($rightSlot, 300);
+		leftTimer = chooseSlotItem($leftSlot, 5000);
+		middleTimer = chooseSlotItem($middleSlot, 5000);
+		rightTimer = chooseSlotItem($rightSlot, 5000);
 	});
 
 	$leftButton.on("click", function(){
