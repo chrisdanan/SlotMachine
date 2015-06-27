3d3f9848d01ce26ab0e2202feb953f3291dca4c4
Christopher Danan <chrisddan@gmail.com>
2015-06-26 23:35:56 -0700

    Add a button that spins the slots, and add code that stops each slot when a button is pressed.

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,4 +1,5 @@
-var slotElements = [5, 6, 7, 8, 9];
+var slotElements = [5, 6, 7, 8, 9],
+	leftTimer, middleTimer, rightTimer;
 
 var randomElement = function(){
 	//Reference: http://stackoverflow.com/questions/1527803/generating-random-numbers-in-javascript-in-a-specific-range
@@ -8,10 +9,12 @@ var randomElement = function(){
 };
 
 var chooseSlotItem = function(slot, interval){
-	window.setInterval(function(){
+	slotTimer = setInterval(function(){
 		slot.empty();
 		slot.append($("<p>").text(randomElement()));
 	}, interval);
+
+	return slotTimer;
 };
 
 var main = function(){
@@ -22,24 +25,46 @@ var main = function(){
 		$rightSlot = $("#rightSlotItem"),
 		$leftButton = $("#leftButton"),
 		$middleButton = $("#middleButton"),
-		$rightButton = $("#rightButton");
+		$rightButton = $("#rightButton"),
+		$spinButton = $("#spinButton");
 
-	chooseSlotItem($leftSlot, 200);
-	chooseSlotItem($middleSlot, 100);
-	chooseSlotItem($rightSlot, 300);
+	$spinButton.on("click", function(){
+		$leftSlot.toggleClass("spinning");
+		$middleSlot.toggleClass("spinning");
+		$rightSlot.toggleClass("spinning");
+
+		leftTimer = chooseSlotItem($leftSlot, 200);
+		middleTimer = chooseSlotItem($middleSlot, 100);
+		rightTimer = chooseSlotItem($rightSlot, 300);
+	});
 
 	$leftButton.on("click", function(){
 		console.log("Left Stop button clicked");
+		if($leftSlot.hasClass("spinning")){
+			$leftSlot.toggleClass("spinning");
+			clearInterval(leftTimer);
+		}
 	});
 
 	$middleButton.on("click", function(){
 		console.log("Middle Stop button clicked");
+		if($middleSlot.hasClass("spinning")){
+			$middleSlot.toggleClass("spinning");
+			clearInterval(middleTimer);
+		}
 	});
 
 	$rightButton.on("click", function(){
 		console.log("Right Stop button clicked");
+		if($rightSlot.hasClass("spinning")){
+			$rightSlot.toggleClass("spinning");
+			clearInterval(rightTimer);
+		}
 	});
 };
 
 
 $(document).ready(main);
+
+//t
+//fflvd
